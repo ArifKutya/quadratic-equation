ac8281db75f72bcbc7c2329b0f2c1c87057c9a42
Arif <arifkutq63@gmail.com>
2023-03-08 08:19:19 +0200

    josephus algotithm

--- src/Josephus/Josephus.php
+++ src/Josephus/Josephus.php
@@ -4,5 +4,17 @@ namespace Arif\SquadEquation\Josephus;
 
 class Josephus
 {
+    public function josephus($n): int
+    {
+        $result = 0;
+        for ($i = 2; $i <= $n; $i++) {
+            $result = ($result + 3) % $i;
+        }
+        return $result +1;
+    }
 
 }
+$josepshus_position = new Josephus();
+$josepshus_position = $josepshus_position->josephus('4');
+echo $josepshus_position;
+
